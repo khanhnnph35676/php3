c38b14e8379599a4955fe47418bb53fd44aeca92
KhanhNhu <khanhnnph35676@fpt.edu.vn>
2024-07-17 16:05:49 +0700

    Buổi 6+7, Model, Migration,Factory,Seeder

---
+++ baitap/database/factories/ProductFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word,
+            'price' => $this->faker->randomFloat(2,1,100),
+            'view' => $this->faker->randomNumber,
+            'created_at' => $this->faker->dateTime,
+            'updated_at' => $this->faker->dateTime,
+        ];
+    }
+}
